7485c15526a83d9957b617f63cce1ff4c47095ed
kelizav <163517774+kelizav@users.noreply.github.com>
2024-05-26 01:14:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] initialArray = { "Russia", "aaa", "bb", "Italy", "11", "Kazakhstan" };
+
+        int count = 0;
+        foreach (string currentString in initialArray)
+        {
+            if (currentString.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] outputArray = new string[count];
+
+        int index = 0;
+        foreach (string currentString in initialArray)
+        {
+            if (currentString.Length <= 3)
+            {
+                outputArray[index] = currentString;
+                index++;
+            }
+        }
+
+        Console.WriteLine("Output:");
+        foreach (string currentString in outputArray)
+        {
+            Console.WriteLine(currentString);
+        }
+    }
+}
